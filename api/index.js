d02c11022148121fe2fb47e559b520f9897e6fb8
sudharsansubash2002 <sudharsan@bosonlabs.net>
2024-12-18 15:57:39 +0530

    objidfix3

--- api/index.js
+++ api/index.js
@@ -115,12 +115,6 @@ const secretKeyGenerator = (private_key) => {
         }
     });
 
-    app.get('/txdetails/:txdigest', async (req, res) => {
-        try {
-            // Fetch the transaction details using the transaction digest
-            const transactionDetails = await client.getTransactionBlock({ tx_digest });
-        }catch(e){}})
-
     app.get('/fetch-object/:objectId', async (req, res) => {
         try {
             const objectId = req.params.objectId;
